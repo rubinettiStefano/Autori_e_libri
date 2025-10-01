d96a38a9241565fadbd1c14c46e7a490f553bd50
StefanoR <rubinetti.stefano@gmail.com>
2025-10-01 14:08:44 +0200

    Initial commit

---
+++ src/main/java/com/generation/autori_e_libri/AutoriELibriApplication.java
@@ -0,0 +1,13 @@
+package com.generation.autori_e_libri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoriELibriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoriELibriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/generation/autori_e_libri/AutoriELibriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.autori_e_libri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoriELibriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
