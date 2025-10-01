c2833f170a445fcf50c401e490a7cf44cf06cf9d
StefanoR <rubinetti.stefano@gmail.com>
2025-10-01 14:35:39 +0200

    aggiunta prima consegna, TODO da 1 a 5

--- src/main/java/com/generation/autori_e_libri/model/entities/Author.java
+++ src/main/java/com/generation/autori_e_libri/model/entities/Author.java
@@ -1,8 +1,30 @@
 package com.generation.autori_e_libri.model.entities;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//TODO 2 - Annotare qui Author
 public class Author
 {
-    //COMMENTO IMPORTANTE
-    private String paperino;
+
+    //TODO 3 - Creare proprietà
+
+    //TODO 4 - Impostare relazione
+    //la cascade deve essere ALL
+    private Set<Book> books = new HashSet<>();
+
+    //TODO 5 - Creare metodi
+    public int numbersOfBooks()
+    {
+        return 0;
+    }
+
+    public int numbersOfBooksInStocks()
+    {
+        return 0;
+    }
+
 
 }
---
+++ src/main/java/com/generation/autori_e_libri/model/entities/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.generation.autori_e_libri.model.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
