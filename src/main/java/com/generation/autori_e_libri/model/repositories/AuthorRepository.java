e4a4f88d202428ea4c72c10064cf0d818693350c
StefanoR <rubinetti.stefano@gmail.com>
2025-10-03 11:53:08 +0200

    test aggiunti

--- src/main/java/com/generation/autori_e_libri/model/repositories/AuthorRepository.java
+++ src/main/java/com/generation/autori_e_libri/model/repositories/AuthorRepository.java
@@ -2,8 +2,13 @@ package com.generation.autori_e_libri.model.repositories;
 
 import com.generation.autori_e_libri.model.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    @Query("select a.id FROM Author a WHERE a.fullName=:nome")
+    UUID dammiIdAutore(String nome);
+
 }
